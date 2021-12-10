1830b939bf3636ca2c75bcd81e892b849e1d64e7
Argy <itsargy@gmail.com>
2021-12-10 12:04:46 -0300

    page 4 added

---
+++ paginas/4/script.js
@@ -0,0 +1,21 @@
+function tabuada() {
+  var num = document.getElementById("txt_n");
+  var tab = document.getElementById("sel_tab");
+
+  if (num.value.length == 0) {
+    window.alert("Preencha os dados corretamente");
+  }
+  
+  else {
+    var n = Number(num.value);
+    tab.innerHTML = ''
+
+    for( c = 1; c < 11 ; c++ ){
+
+        var item = document.createElement('option')
+        item.text = `${n} x ${c} = ${n*c}`
+        tab.appendChild(item);
+    }
+
+  }
+}
